d3bc1ef82bf4f91d8d9846875382ccd7eb002b1f
Chu Quang Hưng <137665099+Achootrain@users.noreply.github.com>
2024-08-28 22:42:31 +0700

    chang port 2

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express();
 const db= require('./models');
 app.use(express.json());// parse the data sent by client in json format
 const cors=require('cors');
-app.use(cors());
-
+app.use(cors({
+    origin: 'your-client-app-url', // Replace with your client app's URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 //ROUTERS
 const basicsRouter = require('./routes/basics');// case-insensitive on windows sever
 app.use('/basics', basicsRouter); // request sent to postRouter->finding the route at Posts->response
@@ -76,9 +79,9 @@ const feedbackRouter=require('./routes/Feedback');
 app.use('/Feedback', feedbackRouter);
 
 db.sequelize.sync().then(() => {
-//listen : the app listens for incoming requests at port 3001 ()
-app.listen( process.env.PORT,"0.0.0.0", () => {
-    console.log("Sever running on port 3001")
-});
+    const PORT = process.env.PORT || 3001; // Fallback to 3001 if PORT is not defined
+    app.listen(PORT, "0.0.0.0", () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 })
 
